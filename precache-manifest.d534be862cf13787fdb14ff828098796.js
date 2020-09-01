edbe7359117cfaba1785737a771c3d7a36268f22
Ernest0203 <ernest2332@gmail.com>
2020-09-01 04:49:51 +0300

    Updates

---
+++ precache-manifest.d534be862cf13787fdb14ff828098796.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689016763e01eb04c29af9b2b661cb14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c3f681b48a76723c35a9",
+    "url": "./static/css/main.42c6e2dd.chunk.css"
+  },
+  {
+    "revision": "94e4a3700d6312796b84",
+    "url": "./static/js/2.a6f6fb79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a6f6fb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f681b48a76723c35a9",
+    "url": "./static/js/main.daafaa08.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "./static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "./static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "8289353d3e3be4974e38dcbc728c48b5",
+    "url": "./static/media/icon.8289353d.svg"
+  }
+]);
